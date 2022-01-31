aef8020d09af44ab94b587628777830afa1fb24d
AaronRBetts <aaronrbetts@gmail.com>
2022-01-31 14:17:05 +0200

    first commit git push heroku master

---
+++ src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './About.css';
+import background from '../../background.png'
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faTwitter } from '@fortawesome/free-brand-svg-icons'
+
+const About = () => {
+  return (
+
+    <section id="about">
+      <div className="about">
+      <h1>What is Padel</h1>
+      <p>Padel is a racquet sport that combines the elements of <b>tennis</b> and <b>squash.</b> It is only played in doubles and is practiced outdoors as much as indoors.</p><br />
+      <p>
+      The sport became very popular in Spain, which has been the professional circuit host of the World Padel Tour where it has been played since 2005.</p><br />
+      <p> Over the course of the past 10 years, padel has begun to spread rapidly to the rest of Europe and the United States.
+</p><br />
+      <p>
+Extremely fun, social and accessible for all, padel is a breath of fresh air that is revitalizing clubs with an activity complimentary to tennis.</p><br />
+        <iframe src="https://www.youtube.com/embed/E2YEYRI_mpk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />
+        <iframe src="https://www.youtube.com/embed/cR0gJapEFbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+      <h1>Follow Us On Social Media</h1>
+
+      <div class="social">
+        {/* <a href="https://instagram.com/padelauckland" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a> */}
+        <a href="" target="_blank"><i class="fab fa-facebook fa-3x"></i></a>
+        <a href="" target="_blank"><i class="fab fa-github fa-3x"></i></a>
+        <a href="" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
+        </div>
+      </div>
+      <div className='imageContainer'>
+        <img className="background" src={background}/>
+      </div>
+
+    </section>
+
+  );
+};
+
+export default About;
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Landing.css'
 import ImagePlayer from '../ImagePlayer/ImagePlayer';
-import Logo from '../../PadelAucklandalt.svg'
+import Logo from '../../PadelAuckland.svg'
+import About from '../About/About'
 
 const Landing = () => {
   return (
@@ -12,29 +13,14 @@ const Landing = () => {
         <video src="https://www.youtube.com/embed/E2YEYRI_mpk" autoplay muted loop></video>
       </div>
       <div class="content">
+        <h1></h1>
         <img className="logo" src={Logo}/>
-        <h1>The fastest growing sport in the world</h1>
+        <h3><i>"Padel combines elements of tennis and squash.. it's easy to start but difficult to master"</i></h3>
         <a href="#about" class="btn">Learn More</a>
       </div>
     </section>
 
-    <section id="about">
-      <h1>What is Padel</h1>
-      <iframe src="https://www.youtube.com/embed/E2YEYRI_mpk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br />
-      <iframe src="https://www.youtube.com/embed/cR0gJapEFbQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <p>
-        {/* enter description */}
-      </p>
-
-      <h2>Follow Us On Social Media</h2>
-
-      <div class="social">
-      <a href="https://instagram.com/padelauckland" target="_blank"><i class="fab fa-instagram fa-3x"></i></a>
-        <a href="" target="_blank"><i class="fab fa-facebook fa-3x"></i></a>
-        <a href="" target="_blank"><i class="fab fa-github fa-3x"></i></a>
-        <a href="" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
-      </div>
-    </section>
+    <About />
   </div>
   )
 };
