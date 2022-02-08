6fdcd0bb111fef7f612c5f12d5c3e42dbde9806a
AaronRBetts <aaronrbetts@gmail.com>
2022-02-08 14:17:11 +0200

    working nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
-import Logo from '../../PadelAucklandalt.svg'
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu)
+  }
 
     return (
-    <nav role="navigation" className="nav">
+    <nav role="navigation" className={toggleMenu ? 'nav expanded-mobile-menu': 'nav'}>
   
-        <a className="menu-toggle" href="#head-nav">
-          <span aria-hidden="true" className="icon-menu"></span><span className="menu-toggle-text"> menu</span>
-        </a>
-            <ul id="main-menu" className="top-nav menu clearfix">
-              <li className='menu-item'>
+        <button className="menu-toggle" onClick={toggleNav}>
+          <FontAwesomeIcon icon={faBars} />
+          <span aria-hidden="true" className="icon-menu"></span><span className="menu-toggle-text"></span>
+        </button>
+            <ul id="main-menu" className={toggleMenu ? 'top-nav menu clearfix expanded-mobile-menu': 'top-nav menu clearfix'}>
+              <li className='menu-item' onClick={toggleNav}>
                 <Link className='link animate__animated animate__fadeIn' to="/">Home</Link>
               </li>
-              <li className='menu-item'>
+              <li className='menu-item' onClick={toggleNav}>
                 <Link className='link animate__animated animate__fadeIn' to="/contact">Contact</Link>
               </li>
-              <li className='menu-item'>
+              <li className='menu-item' onClick={toggleNav}>
                 <Link className='link animate__animated animate__fadeIn' to="/location">Location</Link>
               </li>
-              <li className='menu-item'>
+              <li className='menu-item' onClick={toggleNav}>
                 <Link className='link animate__animated animate__fadeIn' to="/equipment">Information</Link>
                 <ul className="sub-menu">
-                  <li className='menu-item'>
+                  <li className='menu-item' onClick={toggleNav}>
                     <Link className='link animate__animated animate__fadeIn' to="/rules">Rules</Link>
                   </li>
-                  <li className="menu-item">
+                  <li className="menu-item" onClick={toggleNav}>
                     <Link className='link animate__animated animate__fadeIn' to="/equipment">Equipment</Link>
                   </li>
-                  <li className='menu-item'>
+                  <li className='menu-item' onClick={toggleNav}>
                     <Link className='link animate__animated animate__fadeIn' to="/court">Court</Link>
                   </li>
                 </ul>
