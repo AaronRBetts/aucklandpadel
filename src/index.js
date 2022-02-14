96efe81d39936e4538a80e51f016ddd8e7db5cac
AaronRBetts <aaronrbetts@gmail.com>
2022-02-14 11:23:26 +0200

    SEO configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';import { hydrate, render } from "react-dom";
+
+
+const APP = (
+    <App />
+)
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
@@ -10,13 +14,6 @@ if (rootElement.hasChildNodes()) {
 } else {
   render(APP, rootElement);
 }
-
-const APP = (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
