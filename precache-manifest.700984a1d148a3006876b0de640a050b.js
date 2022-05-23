fec87897c166fa6e81443e365358a3db2e79e9a1
AaronRBetts <AaronRBetts@gmail.com>
2022-05-23 14:43:38 +0300

    Updates

---
+++ precache-manifest.700984a1d148a3006876b0de640a050b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b166460be9a1c5f927dd",
+    "url": "/static/css/main.e70a72eb.chunk.css"
+  },
+  {
+    "revision": "b166460be9a1c5f927dd",
+    "url": "/static/js/main.b166460b.chunk.js"
+  },
+  {
+    "revision": "6d901b3e33806cc9ca52",
+    "url": "/static/js/1.6d901b3e.chunk.js"
+  },
+  {
+    "revision": "484b2d04907b201fadef",
+    "url": "/static/css/2.75394a6a.chunk.css"
+  },
+  {
+    "revision": "484b2d04907b201fadef",
+    "url": "/static/js/2.484b2d04.chunk.js"
+  },
+  {
+    "revision": "2600f86dfe95919d6557",
+    "url": "/static/js/runtime~main.2600f86d.js"
+  },
+  {
+    "revision": "f82a43d95a90bc42d80185b7c1896e2b",
+    "url": "/static/media/logo_small.f82a43d9.png"
+  },
+  {
+    "revision": "219cf1342ebaf9e3f554cf319dfb35e1",
+    "url": "/static/media/PadelAuckland.219cf134.svg"
+  },
+  {
+    "revision": "6ed03ede94ed1232a92e00d016d44350",
+    "url": "/static/media/background.6ed03ede.png"
+  },
+  {
+    "revision": "56fb9acf259615c0e464c425e8b7b61a",
+    "url": "/static/media/pageBanner.56fb9acf.png"
+  },
+  {
+    "revision": "81a73e0ea88124800c01c8ef413e8843",
+    "url": "/static/media/padelCourt.81a73e0e.png"
+  },
+  {
+    "revision": "b2203eb0f91e7077d6ba1744bfaf1f2d",
+    "url": "/static/media/PadelAucklandalt.b2203eb0.svg"
+  },
+  {
+    "revision": "ae253117718ab6ac331299066c527e0c",
+    "url": "/index.html"
+  }
+];
